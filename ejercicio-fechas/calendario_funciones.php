43949b93717d55e0410b4e1663bbdf26e878ce9c
Guillermo <guilleleongordo@gmail.com>
2024-10-13 21:14:06 +0200

    back up the donkey

--- ejercicio-fechas/calendario_funciones.php
+++ ejercicio-fechas/calendario_funciones.php
@@ -1,23 +1,53 @@
 <?php
-function calendario_mensual($año,$mes) {
+function calendario_mensual($ano,$mes) {
     if ($mes>12||$mes<1) {
         throw new Exception("Error En el mes introducido", 1);
         
     }
+$res='';
+    $res.="<table>";
+    $res.="<caption>".date("F-Y",strtotime("1-".$mes."-".$ano))."</caption>";
+$res.='<thead><tr>';
+$res.='<th>L</th>';
+$res.='<th>M</th>';
+$res.='<th>X</th>';
+$res.='<th>J</th>';
+$res.='<th>V</th>';
+$res.='<th>S</th>';
+$res.='<th>D</th>';
+$res.='</tr></thead>';
+$res.='<tbody>';
+$days=$mes == 2 ? ($ano % 4 ? 28 : ($ano % 100 ? 29 : ($ano % 400 ? 28 : 29))) : (($mes - 1) % 7 % 2 ? 30 : 31);
+$res.='<tr>';
+for ($i=1; $i <= $days; $i++) { 
+    if ($i%7==1) {
+        $res.='</tr><tr>';
+    }
+    $res.='<td>'.$i.'</td>';
+}
+$res.='</tr>';
+$res.='</tbody>';
+    $res.="</table>";
+    return $res;
+}
 
-    print("<table>");
-    print("<caption>".date("F-Y",strtotime("1-".$mes."-".$año))."</caption>");
-print('<thead><tr>');
-print('<th>L</th>');
-print('<th>M</th>');
-print('<th>X</th>');
-print('<th>J</th>');
-print('<th>V</th>');
-print('<th>S</th>');
-print('<th>D</th>');
-print('</tr></thead>');
+//================================================
 
-    print("</table>");
+function calendario_anual($ano){
+print('<div class="diss">');
+print('<table>');
+print("<caption>Calendario ".$ano."</caption>");
+
+print('<tbody><tr>');
+for ($i=1; $i < 13; $i++) { 
+    if ($i%4==1) {
+        print('</tr><tr>');
+    }
+    print('<td><div>'.calendario_mensual($ano,$i).'</div></td>');
+}
 
 
+print('</tr></tbody>');
+print('</table>');
+print('</div>');
 }
--- ejercicio-fechas/index-fechas.php
+++ ejercicio-fechas/index-fechas.php
@@ -10,12 +10,16 @@
     <?php
     include(__DIR__."/calendario_funciones.php");
     print("<div class=\"info\"");
+    print('<h1>Ejercicio 1</h1>');
     date_default_timezone_set('Europe/Madrid');
     print("<p>Fecha de hoy: ".date("d-m-Y",time())."</p>");
     print("<p>Fecha de mañana: ".date('d-m-Y',mktime(0,0,0,date('m',time()),date('d',time())+1,date('y',time())))."</p>");
     print("<p>Hora actual: ".date("G-i-s",time())."</p>");
     print("<p>Fecha del próximo Lunes: ".date("d-m-Y", strtotime("next monday"))."</p>");
-    calendario_mensual(2024,5);
+    print('<h1>Ejercicio 2</h1>');
+    print(calendario_mensual(2024,5));
+    print('<h1>Ejercicio 3</h1>');
+    calendario_anual(2001);
     print("</div>");
     
     ?>
