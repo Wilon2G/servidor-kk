949db36d008fd534466db9fc75f342f4fa50322a
guille <guilleleongordo@gmail.com>
2025-01-17 11:09:27 +0100

    kk

--- ejercicio-xml/utils.php
+++ ejercicio-xml/utils.php
@@ -2,27 +2,27 @@
 include 'ejemplo.php';
 //$xml = simplexml_load_string($xmlstr);
 $xml = @simplexml_load_file("data.xml");
-function showAll(){
+function showAll()
+{
     global $xml;
     foreach ($xml->evento as $evento) {
-        print("<div class=\"evento\">");
-        print("<div class=\"imgContainer\"><img style=\"width: 100%\" src=\"".$evento->caratula."\" alt=\"img not found\"></div>");
-        
-        print("<div class=\"infoContainer\">");
-        print("<h3>".$evento->titulo["value"]."</h3>"
-        ."<a href=\"".$evento->compra."\" ><button>Compra tus entradas!</button></a>"
-        ."<p>Calificación: ".$evento->calificacion."</p>"
-        ."<p>Género: ".$evento->genero."</p>"
-        ."<p>Duración: ".$evento->duracion."min</p>"
-        ."<div><p>Sinopsis:</p><p>".$evento->sinopsis."</p></div>"
-        ."<iframe src=\"".$evento->trailer."\" width=\"300\" height=\"200\" ></iframe>"
+        print ("<div class=\"evento\">");
+        print ("<div class=\"imgContainer\"><img src=\"" . $evento->caratula . "\" alt=\"img not found\"></div>");
 
+        print ("<div class=\"infoContainer\">");
+        print ("<h3 style=\"width: fit-content;\" >" . $evento->titulo["value"] . "</h3>"
+            . "<a style=\"width: fit-content;\" href=\"" . $evento->compra . "\" ><button>Compra tus entradas!</button></a>"
+            . "<p>Calificación: " . $evento->calificacion . "</p>"
+            . "<p>Género: " . $evento->genero . "</p>"
+            . "<p>Duración: " . $evento->duracion . "min</p>"
+            . "<div class=\"sinopsis\" ><p>Sinopsis:</p><p>" . $evento->sinopsis . "</p></div>"
+            . "<a style=\"width: fit-content;\" href=\"" . $evento->trailer . "\" ><button>Tráiler</button></a>"
+        );
+        print ("</div>");
 
-        
-    );
-    print("</div>");
+        print("<button class=\"edit\" >Edit</button>");
 
-        print("</div><br>");
+        print ("</div><br>");
     }
 }
 
