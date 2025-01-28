d79513cf74fc8e11efec93fbc5233771c49408ac
guille <guilleleongordo@gmail.com>
2025-01-28 14:06:22 +0100

    kk

--- laravel-diez/routes/web.php
+++ laravel-diez/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Para ejecutar en comando hacemos cd a la carpeta y ejecutamos php artisan serve
+
+Route::get('/kk', function () {
+    return "<h1>KKKKKK</h1>";
+});
